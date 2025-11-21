83fca81320f4a46837dd5334afe5dae3af516f8b
xhath <ayammail82@gmail.com>
2025-11-21 21:45:31 +0700

    Bias Correction with Delta Method
    Using Delta method to reduce model error in R language

---
+++ delta_biascorrection.R
@@ -0,0 +1,9 @@
+delta_cor <- function(dfobs, dfbaseline, dfmodel){
+  obs <- dfobs$CH
+  baseline <- dfbaseline$CH
+  model <- as.numeric(dfmodel$CH)
+  
+  ratio <- model / baseline
+  corrected <- ratio * obs
+ 
+}
