943c9878d350612394b1890d1a23c04c96d1b730
xhath <ayammail82@gmail.com>
2025-12-06 17:16:59 +0700

    making outputpath

--- makingwrf_neededvar.R
+++ makingwrf_neededvar.R
@@ -1,9 +1,11 @@
-membuatncdf <- function(wrf,outputpath_rain,outputpath_norain){
+membuatncdf <- function(wrf,outputpath){
   
   library(ncdf4)
   library(tidyverse)
-  library(stars)
+  #library(stars)
   wrf <- nc_open(wrf)
+  on.exit(nc_close(wrf))
+  
   long <- ncvar_get(wrf,'XLONG')
   lat <- ncvar_get(wrf,'XLAT')
   
@@ -13,15 +15,21 @@ membuatncdf <- function(wrf,outputpath_rain,outputpath_norain){
   
   date <- dim(date)[1]
   
+  #timez <- ncvar_get(wrf,'Times')
+  #timez <- timez[]
+  
   rainc <- ncvar_get(wrf, 'RAINC')
   rainnc <- ncvar_get(wrf, 'RAINNC')
   rain <- rainc + rainnc
+  rain[rain > 1e30] <- 0
+  rain[rain>1000]
+  rain[rain < 0] <- 0
   rain_diff <- rain
   for(i in 2:dim(rain)[3]) {
     rain_diff[,,i] <- rain[,,i] - rain[,,i-1]
     }
-  rain_diff[rain_diff < 0] <- 0
-  hourly_rain <- rain_diff
+  #rain_diff[rain_diff < 0] <- 0
+  hourly_rain <- as.numeric(rain_diff)
 
   T2 <- ncvar_get(wrf,'T2')-273.15
   T2M <- T2
@@ -43,27 +51,30 @@ membuatncdf <- function(wrf,outputpath_rain,outputpath_norain){
   ws <- 0.622*es/(psfc - es)
   rh <- qv2/ws * 100
   
-  rain_date <- dim(hourly_rain)[3]
+  #rain_date <- dim(hourly_rain)[3]
   
   longitude <- ncdim_def('longitude','degree',vals = long)
   latitude <- ncdim_def('latitude','degree',vals = lat)
-  time_date_rain <- ncdim_def('time','hourly',vals = 1:rain_date)
+  #time_date_rain <- ncdim_def('time','hourly',vals = 1:rain_date)
   time_date <- ncdim_def('time','hourly',vals=1:date)
   
-  var_rain <- ncvar_def('rain','mm/hour',dim=list(longitude,latitude,time_date_rain))
+  var_rain <- ncvar_def('rain','mm/hour',dim=list(longitude,latitude,time_date))
   
   var_t2m    <- ncvar_def('t2m', 'degC', list(longitude,latitude,time_date))
   var_sftemp <- ncvar_def('surface_temp', 'degC', list(longitude,latitude,time_date))
   var_ws10m  <- ncvar_def('windspeed_10m', 'm/s', list(longitude,latitude,time_date))
   var_rh     <- ncvar_def('rh', '%', list(longitude,latitude,time_date))
+  #var_time <- ncvar_def('time','date',list(as.numeric(timez)))
   
-  nc_norain <- nc_create(outputpath_norain,vars=list(var_rh,var_ws10m,var_sftemp,var_t2m))
-  nc_rain <- nc_create(outputpath_rain,vars=list(var_rain))
+  nc_fixed <- nc_create(outputpath,vars=list(var_rain,var_rh,var_ws10m,var_sftemp,var_t2m))
+  #nc_rain <- nc_create('C:/Users/HP/Downloads/nc_.nc',vars=list(var_rain))
   
-  ncvar_put(nc_rain,var_rain,hourly_rain)
-  ncvar_put(nc_norain,var_sftemp,SFTEMP)
-  ncvar_put(nc_norain,var_rh,rh)
-  ncvar_put(nc_norain,var_t2m,T2M)
-  ncvar_put(nc_norain,var_ws10m,ws10m)
+  ncvar_put(nc_fixed,var_rain,hourly_rain)
+  ncvar_put(nc_fixed,var_sftemp,SFTEMP)
+  ncvar_put(nc_fixed,var_rh,rh)
+  ncvar_put(nc_fixed,var_t2m,T2M)
+  ncvar_put(nc_fixed,var_ws10m,ws10m)
+  
+  nc_close(nc_fixed)
   
 }
